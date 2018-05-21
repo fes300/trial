f01f088219e6638479b4588c28fbc888947346a3
fes300 <federicosordillo@gmail.com>
2018-05-21 09:52:11 +0200

    initial commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export interface AppProps { compiler: string; framework: string; }
+
+// 'AppProps' describes the shape of props.
+// State is never set so we use the '{}' type.
+export class App extends React.Component<AppProps, {}> {
+  render() {
+    return (
+      <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+    alias: {
+      '@components': path.resolve(__dirname, './src/components')
+    }
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM",
+  },
+}
